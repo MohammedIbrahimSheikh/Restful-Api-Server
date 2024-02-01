c088d3e29567ecc3b6f5aa7c8000f55ab162e782
MohammedIbrahimSheikh <ibrahim03178871902@gmail.com>
2024-02-01 11:24:57 +0500

    server updated

--- server.mjs
+++ server.mjs
@@ -1,5 +1,6 @@
 import express from 'express'
 import cors from 'cors';
+import { nanoid } from 'nanoid';
 const app = express()
 
 app.use(cors()); // have got to cover later
@@ -18,20 +19,64 @@ const port =process.env.PORT || 3000 ; // searches an available port at heroku
 
 let users =[];
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+
+app.get('/user/:userId',(req,res)=>{
+  let userId = req.params.userId;
+  let isFound = false;
+  for (let i = 0; i < users.length; i++) {
+    if (userId == users[i].Id){
+      isFound = true;
+      res.send(users[i]);
+      break;
+    }
+  }
+  if (!isFound) res.send("User doesn't exist !");
 })
 
+app.get('/users',(req,res) => res.send(users));
 
 app.post('/user',(req,res)=>{
   users.push(req.body);
-  console.log("post ke request",req.body);
+  users[(users.length - 1)].Id = nanoid();
   res.send("Congrats! User has been Created");
 })
 
-app.get('/user',(req,res)=>{
-  res.send(users);
-  console.log("get ke request",req.body);
+app.put('/user/:userId',(req,res) => {
+  let userId = req.params.userId;
+  let isFound = false;
+  // console.log( usersId , isFound);
+  for (let i = 0; i < users.length; i++) {
+    if (userId == users[i].Id) {
+
+      isFound = true;
+
+      if(req.body.firstname) users[i].firstname = req.body.firstname;
+
+      if(req.body.lastname) users[i].lastname = req.body.lastname;
+
+      if(req.body.email) users[i].email = req.body.email;
+
+      if(req.body.password) users[i].password = req.body.password;
+
+      res.send("Congrats! User has been updated.");
+      break;
+    }
+  }
+  if(!isFound) res.send("User doesn't exist !");
+})
+
+app.delete('/user/:userId',(req,res) => {
+
+  let userId = req.params.userId ; 
+  let isFound = false;
+  for (let i = 0; i < users.length; i++) {
+    if (userId == users[i].Id) {
+     users.splice(i,1);
+      res.send("User has been deleted!");
+      break;
+    }
+  }
+  if(!isFound) res.send("User doesn't exist !");
 })
 
 app.listen(port, () => {
